1e460624baa57bc4c4dc3dde064d8ea4ace852ef
nicohman <nico.hickman@gmail.com>
2018-05-28 17:47:52 -0700

    Add correct publish dates, add descriptions

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,9 @@ use select::predicate::{Name, And, Class, Descendant};
 const FILE_USE: bool = true;
 struct Book {
     title:String,
-    start:String
+    start:String,
+    desc:String,
+    date:String,
 }
 struct DownloadedBook {
     title:String,
@@ -41,27 +43,39 @@ fn get_info(key:&str) -> Book{
     return match key {
         "worm" => Book {
             title:String::from("Worm"),
-            start:String::from("parahumans.wordpress.com/2011/06/11/1-1/")
+            start:String::from("parahumans.wordpress.com/2011/06/11/1-1/"),
+            desc:String::from("An introverted teenage girl with an unconventional superpower, Taylor goes out in costume to find escape from a deeply unhappy and frustrated civilian life. Her first attempt at taking down a supervillain sees her mistaken for one, thrusting her into the midst of the local ‘cape’ scene’s politics, unwritten rules, and ambiguous morals. As she risks life and limb, Taylor faces the dilemma of having to do the wrong things for the right reasons."),
+            date:String::from("Tue, 19 Nov 2013 00:00:00 +0100"),
         },
         "pact" => Book {
             title:String::from("Pact"),
-            start:String::from("pactwebserial.wordpress.com/category/story/arc-1-bonds/1-01/")
+            start:String::from("pactwebserial.wordpress.com/category/story/arc-1-bonds/1-01/"),
+            desc:String::from("Blake Thorburn was driven away from home and family by a vicious fight over inheritance, returning only for a deathbed visit with the grandmother who set it in motion.   Blake soon finds himself next in line to inherit the property, a trove of dark supernatural knowledge, and the many enemies his grandmother left behind her in the small town of Jacob’s Bell."),
+            date:String::from("Sat, 07 Mar 2015 00:00:00 +0100"),
         },
         "twig" => Book {
             title:String::from("Twig"),
-            start:String::from("twigserial.wordpress.com/2014/12/24/taking-root-1-1/")
+            start:String::from("twigserial.wordpress.com/2014/12/24/taking-root-1-1/"),
+            desc:String::from("The year is 1921, and a little over a century has passed since a great mind unraveled the underpinnings of life itself.  Every week, it seems, the papers announce great advances, solving the riddle of immortality, successfully reviving the dead, the cloning of living beings, or blending of two animals into one.  For those on the ground, every week brings new mutterings of work taken by ‘stitched’ men of patchwork flesh that do not need to sleep, or more fearful glances as they have to step off the sidewalks to make room for great laboratory-grown beasts.  Often felt but rarely voiced is the notion that events are already spiraling out of the control of the academies that teach these things. It is only this generation, they say, that the youth and children are able to take the mad changes in stride, accepting it all as a part of day to day life.  Of those children, a small group of strange youths from the Lambsbridge Orphanage stand out, taking a more direct hand in events."),
+            date:String::from("Tue, 17 Oct 2017 00:00:00 +0200"),
         },
         "glow" => Book {
             title:String::from("Glow-worm"),
-            start:String::from("parahumans.wordpress.com/2017/10/21/glowworm-p-1/")
+            start:String::from("parahumans.wordpress.com/2017/10/21/glowworm-p-1/"),
+            desc:String::from("The bridge between Worm and Ward, Glow-worm introduces readers to the characters of Ward, and the consequences of Gold Morning"),
+            date:String::from("Sat, 11 Nov 2017 00:00:00 +0100"),
         },
         "ward" => Book {
             title:String::from("Ward"),
-            start:String::from("parahumans.net/2017/09/11/daybreak-1-1/")
+            start:String::from("parahumans.net/2017/09/11/daybreak-1-1/"),
+            desc:String::from("The unwritten rules that govern the fights and outright wars between ‘capes’ have been amended: everyone gets their second chance.  It’s an uneasy thing to come to terms with when notorious supervillains and even monsters are playing at being hero.  The world ended two years ago, and as humanity straddles the old world and the new, there aren’t records, witnesses, or facilities to answer the villains’ past actions in the present.  One of many compromises, uneasy truces and deceptions that are starting to splinter as humanity rebuilds. None feel the injustice of this new status quo or the lack of established footing more than the past residents of the parahuman asylums.  The facilities hosted parahumans and their victims, but the facilities are ruined or gone; one of many fragile ex-patients is left to find a place in a fractured world.  She’s perhaps the person least suited to have anything to do with this tenuous peace or to stand alongside these false heroes.  She’s put in a position to make the decision: will she compromise to help forge what they call, with dark sentiment, a second golden age?  Or will she stand tall as a gilded dark age dawns?"),
+            date:String::from("Sat, 11 Nov 2017 00:00:00 +0100"),
         },
         _ =>  Book {
             title:String::from("Worm"),
-            start:String::from("parahumans.wordpress.com/2011/06/11/1-1/")
+            start:String::from("parahumans.wordpress.com/2011/06/11/1-1/"),
+            desc:String::from("An introverted teenage girl with an unconventional superpower, Taylor goes out in costume to find escape from a deeply unhappy and frustrated civilian life. Her first attempt at taking down a supervillain sees her mistaken for one, thrusting her into the midst of the local ‘cape’ scene’s politics, unwritten rules, and ambiguous morals. As she risks life and limb, Taylor faces the dilemma of having to do the wrong things for the right reasons."),
+            date:String::from("Tue, 19 Nov 2013 00:00:00 +0100"),
         },
 
     };
@@ -96,7 +110,7 @@ fn print_help() {
     println!("ward: Scrapes Ward");
 }
 fn download_book(book:Book) -> DownloadedBook {
-    let elements = vec![BookElement::Name(book.title.clone()), BookElement::Author("John McCrae".to_string()), BookElement::Language("en-US".to_string()), BookElement::Date(DateTime::parse_from_rfc3339("2017-02-08T15:30:18+01:00").unwrap())];
+    let elements = vec![BookElement::Name(book.title.clone()), BookElement::Author("John McCrae".to_string()), BookElement::Language("en-US".to_string()), BookElement::Date(DateTime::parse_from_rfc2822(&book.date).unwrap()), BookElement::StringDescription(book.desc)];
     let client = Client::new();
     if FILE_USE {
         if !fs::metadata("content").is_err() {
